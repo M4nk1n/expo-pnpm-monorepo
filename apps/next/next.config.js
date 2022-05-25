621117a2d48da5b0a90cb847d2392a3c48407a50
Mankin <vinkencheng@gmail.com>
2022-05-25 11:51:46 +0800

    feat: 项目 monorepo 改造

---
+++ apps/next/next.config.js
@@ -0,0 +1,24 @@
+const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
+const withFonts = require('next-fonts');
+const { withExpo } = require('@expo/next-adapter');
+
+const withTM = require('next-transpile-modules')([
+  '@packages/app'
+]);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack5: true
+};
+
+module.exports = withPlugins(
+  [
+    withTM,
+    [withImages, { projectRoot: __dirname }],
+    [withFonts, { projectRoot: __dirname }],
+    [withExpo, { projectRoot: __dirname }],
+  ],
+  nextConfig
+);
