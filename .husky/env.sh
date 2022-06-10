316b0aa69bdbd96ea0eed7ac4ad4245fa63a91a8
Mankin <vinkencheng@gmail.com>
2022-06-10 16:46:57 +0800

    chore: 添加 husky 自动获取运行环境脚本

---
+++ .husky/env.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+export PATH="/opt/homebrew/opt/node@16/bin:$PATH"
+export PATH="$HOME/.yarn/bin:$PATH"
+
+# window平台git hooks有时候可能会失败。
+# 在 Windows 上通过 Git Bash ( stdin is not a tty) 使用 Yarn 时，Git 钩子可能会失败。如果您有 Windows 用户，强烈建议添加以下解决方法。
+command_exists () {
+  command -v "$1" >/dev/null 2>&1
+}
+
+if command_exists winpty && test -t 1; then
+  exec < /dev/tty
+fi
