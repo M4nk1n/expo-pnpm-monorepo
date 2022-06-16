07f9b47412efd38b66147bbf12e7c9020e7a3ecc
Mankin <vinkencheng@gmail.com>
2022-06-16 17:30:07 +0800

    style: format with SingleQuote

--- packages/app/src/navigation/NavigatorProvider.tsx
+++ packages/app/src/navigation/NavigatorProvider.tsx
@@ -1,15 +1,19 @@
-import React from "react"
-import { NavigationContainer, Stack } from "@packages/navigation"
+import React from 'react'
+import { NavigationContainer, Stack } from '@packages/navigation'
 
-import Home from "../screens/home"
-import Device from "../screens/device"
+import Home from '../screens/home'
+import Device from '../screens/device'
 
 export default function NavigatorProvider() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} options={{ title: "主页" }} />
-        <Stack.Screen name="Device" component={Device} options={{ title: "设备", presentation: 'modal', headerShown: false }} />
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={Home} options={{ title: '主页' }} />
+        <Stack.Screen
+          name='Device'
+          component={Device}
+          options={{ title: '设备', presentation: 'modal', headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   )
