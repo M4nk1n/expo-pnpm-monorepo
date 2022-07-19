00d70fe7a6865c43069ac0ed141753951c60fcaf
Mankin <vinkencheng@gmail.com>
2022-07-19 15:48:56 +0800

    feat(packages/components): new component - CheckBox

---
+++ packages/components/src/CheckBox/index.tsx
@@ -0,0 +1,40 @@
+import React, { useMemo } from 'react'
+import { Image, Pressable, StyleSheet, View } from 'react-native'
+
+import iconNormal from './assets/opt_nor.png'
+import iconChecked from './assets/opt_sel.png'
+
+type CheckBoxProps = {
+  size: number
+  checked: boolean
+  onPress: () => void
+  disabled?: boolean
+}
+
+const CheckBox: React.FC<CheckBoxProps> = ({ size, checked, onPress, disabled = false }) => {
+  const styles = useMemo(() => {
+    return StyleSheet.create({
+      view: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      icon: {
+        width: size,
+        height: size,
+        resizeMode: 'contain',
+      },
+    })
+  }, [size])
+
+  return (
+    <Pressable onPress={onPress} disabled={disabled}>
+      <View style={styles.view}>
+        <Image source={checked ? iconChecked : iconNormal} style={styles.icon} />
+      </View>
+    </Pressable>
+  )
+}
+
+export { CheckBox }
