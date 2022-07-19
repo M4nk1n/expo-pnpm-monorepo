e14c29b7765cbcb6fd0da613015341bc086d9de3
Mankin <vinkencheng@gmail.com>
2022-07-19 15:47:10 +0800

    feat(app): new GlobalStyles

--- packages/app/src/themes/index.ts
+++ packages/app/src/themes/index.ts
@@ -1,6 +1,20 @@
+import { StyleSheet } from 'react-native'
 import { Themes } from '@packages/theme'
 
 import light from './light.json'
 import dark from './dark.json'
 
 export const themes: Themes = { light, dark }
+
+export const GlobalStyles = StyleSheet.create({
+  FlexCenter: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Shadow: {
+    shadowColor: '#333',
+    shadowRadius: 2,
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 2, height: 2 },
+  },
+})
