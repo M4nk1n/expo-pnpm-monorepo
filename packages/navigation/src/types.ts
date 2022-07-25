b6706acf87d9fa7f510c4af94353f2a1a7bcf958
Mankin <vinkencheng@gmail.com>
2022-07-25 17:56:26 +0800

    fix(packages/navigation): rename useNavigationProp & useRouteProp
    useNavigationProp -> NavigateProp
    useRouteProp -> RoutePageProp

--- packages/navigation/src/types.ts
+++ packages/navigation/src/types.ts
@@ -2,5 +2,5 @@ import { ParamListBase, RouteProp } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 
-export type useNavigationProp = StackNavigationProp<any> | NativeStackNavigationProp<any>
-export type useRouteProp<T extends ParamListBase> = RouteProp<{ params: T }, 'params'>
+export type NavigateProp = StackNavigationProp<any> | NativeStackNavigationProp<any>
+export type RoutePageProp<T extends ParamListBase> = RouteProp<{ params: T }, 'params'>
