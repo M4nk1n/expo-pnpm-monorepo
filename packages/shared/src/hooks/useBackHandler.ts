220066f7029b03a396283295242b5f08fe6de327
Mankin <vinkencheng@gmail.com>
2022-07-25 17:58:09 +0800

    docs(packages/shared): 补充 useBackHandler 注释

--- packages/shared/src/hooks/useBackHandler.ts
+++ packages/shared/src/hooks/useBackHandler.ts
@@ -1,6 +1,12 @@
 import { useEffect } from 'react'
 import { BackHandler } from 'react-native'
 
+/**
+ * - **If one subscription returns true**, then subscriptions registered earlier will not be called.
+ * - **If no subscription returns true or none are registered**, it programmatically invokes the default back button functionality to exit the app.
+ * ```
+ * @param handler subscription，返回 true 可以阻止调用系统返回事件
+ */
 export function useBackHandler(handler: () => boolean) {
   useEffect(() => {
     BackHandler.addEventListener('hardwareBackPress', handler)
