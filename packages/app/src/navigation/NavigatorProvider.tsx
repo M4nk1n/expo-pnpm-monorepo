e20582f6f10400180d43a9b67bca0bbdcd0fcc1f
Mankin <vinkencheng@gmail.com>
2022-07-26 14:16:34 +0800

    perf(app): wrapping Device route with Stack.Group

--- packages/app/src/navigation/NavigatorProvider.tsx
+++ packages/app/src/navigation/NavigatorProvider.tsx
@@ -8,12 +8,11 @@ export default function NavigatorProvider() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={Home} options={{ title: '主页' }} />
-        <Stack.Screen
-          name='Device'
-          component={Device}
-          options={{ title: '设备', presentation: 'modal', headerShown: false }}
-        />
+        <Stack.Screen name='Home' component={Home} options={{ title: 'Home' }} />
+
+        <Stack.Group screenOptions={{ presentation: 'modal', headerShown: false }}>
+          <Stack.Screen name='Device' component={Device} options={{ title: 'Device' }} />
+        </Stack.Group>
       </Stack.Navigator>
     </NavigationContainer>
   )
