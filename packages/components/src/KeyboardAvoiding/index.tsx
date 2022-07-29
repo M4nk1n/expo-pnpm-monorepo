c5537f0f182733a0b27d3ccf97e4785d7007c4a2
Mankin <vinkencheng@gmail.com>
2022-07-29 15:59:51 +0800

    refactor(@packages/components): 重新封装 KeyboardAvoidingView，新增 KeyboardAvoidingViewProvider

--- packages/components/src/KeyboardAvoiding/index.tsx
+++ packages/components/src/KeyboardAvoiding/index.tsx
@@ -1,25 +1,52 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { KeyboardAvoidingView as RNKeyboardAvoidingView, Platform } from 'react-native'
+import type { StyleProp, ViewStyle } from 'react-native'
 
-export const KeyboardAvoidingViewContext = React.createContext({
-  isPresentedModally: false,
-  isVisible: true,
+export type KeyboardAvoidingViewProps = {
+  isVisible: boolean
+  isPresentedModally: boolean
   /**
    * bottom offset is used when the screen is presented modally to account for
    * any extra padding at the bottom of the screen (i.e. added for safe area insets)
    */
-  bottomOffset: 0,
+  bottomOffset: number
+}
+
+export type KeyboardAvoidingViewContextProps = {
+  value: KeyboardAvoidingViewProps
+  setValue: React.Dispatch<React.SetStateAction<KeyboardAvoidingViewProps>>
+}
+
+export const KeyboardAvoidingViewContext = React.createContext<KeyboardAvoidingViewContextProps>({
+  value: {
+    isVisible: true,
+    isPresentedModally: false,
+    bottomOffset: 0,
+  },
+  setValue: () => {
+    /* do nothing */
+  },
 })
 
-export const KeyboardAvoidingProvider: React.FC = ({ children }) => {
-  const { isVisible } = useContext(KeyboardAvoidingViewContext)
+export const KeyboardAvoidingViewProvider: React.FC = ({ children }) => {
+  const [value, setValue] = useState<KeyboardAvoidingViewProps>({
+    isVisible: true,
+    isPresentedModally: false,
+    bottomOffset: 0,
+  })
 
   return (
-    <RNKeyboardAvoidingView
-      enabled={isVisible}
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={{ flex: 1 }}
-    >
+    <KeyboardAvoidingViewContext.Provider value={{ value, setValue }}>{children}</KeyboardAvoidingViewContext.Provider>
+  )
+}
+
+export const KeyboardAvoidingView: React.FC<{ style?: StyleProp<ViewStyle> }> = ({ children, style }) => {
+  const {
+    value: { isVisible },
+  } = useContext(KeyboardAvoidingViewContext)
+
+  return (
+    <RNKeyboardAvoidingView enabled={isVisible} behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={style}>
       {children}
     </RNKeyboardAvoidingView>
   )
