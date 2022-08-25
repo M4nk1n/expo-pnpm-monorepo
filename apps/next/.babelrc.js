9b68c256a2eb127ccf9b4afcd51b66434914bc0f
Mankin <vinkencheng@gmail.com>
2022-08-25 13:02:03 +0800

    chore(@apps/next): 调整 babelrc配置

--- apps/next/.babelrc.js
+++ apps/next/.babelrc.js
@@ -1,23 +1,20 @@
-module.exports = function (api) {
-  api.cache(true)
-  return {
-    presets: ['@expo/next-adapter/babel'],
-    plugins: [
-      'react-native-web',
-      // https://github.com/expo/expo/issues/14374
-      ['@babel/plugin-proposal-private-methods', { loose: true }],
-      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-      [
-        'module-resolver',
-        {
-          root: ['.'],
-          alias: {
-            '@app': '@packages/app/src',
-          },
+module.exports = {
+  presets: ['@expo/next-adapter/babel'],
+  plugins: [
+    'react-native-web',
+    // https://github.com/expo/expo/issues/14374
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@app': './node_modules/@packages/app/src',
         },
-      ],
-      // Note: If you load other babel plugins, the Reanimated plugin has to be listed last in the plugins array.
-      'react-native-reanimated/plugin',
+      },
     ],
-  }
+    // Note: If you load other babel plugins, the Reanimated plugin has to be listed last in the plugins array.
+    'react-native-reanimated/plugin',
+  ],
 }
