30618413599591ab99403f7c4e4c22555be72320
Mankin <vinkencheng@gmail.com>
2022-09-01 16:33:25 +0800

    feat(@shared/components): 用 ActivityIndicator 作为 Suspense 的默认 fallback 属性

--- shared/components/src/Suspense/index.tsx
+++ shared/components/src/Suspense/index.tsx
@@ -1,10 +1,10 @@
 import React, { PropsWithChildren, Suspense } from 'react'
-import { View } from 'react-native'
+import { ActivityIndicator } from 'react-native'
 
 export interface SuspenseProviderProps {
   fallback?: React.ReactNode
 }
 
 export const SuspenseProvider: React.FC<PropsWithChildren<SuspenseProviderProps>> = ({ fallback, ...props }) => (
-  <Suspense fallback={fallback ?? <View />} {...props} />
+  <Suspense fallback={fallback ?? <ActivityIndicator size='large' />} {...props} />
 )
